cd8b6236fe52e2a3c04217d3eade404d2261159b
samuelasiabaka <samuelasiabaka@gmail.com>
2026-08-08 23:04:47 +0100

    added shape project

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Circle circle = new Circle("Red", 5);
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+
+        Square square = new Square("Green", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+        shapes.Add(square);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} has an area of {shape.GetArea()}");
+        }
     }
 }
